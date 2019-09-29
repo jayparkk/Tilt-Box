3efbf3da01b1a6c6254d2a05dee68cfc1785f998
jayparkk <50163510+jayparkk@users.noreply.github.com>
2019-09-28 20:28:36 -0700

    Add files via upload

---
+++ color.js
@@ -0,0 +1,10 @@
+
+document.getElementById('root').addEventListener('click', changeColor);
+
+function changeColor() {
+    box = document.querySelector('.tilt-root');
+
+    box.style.background = '-webkit-linear-graidient(135deg, ' + '#'+Math.floor(Math.random()*16777215).toString(16) + ' 30%, ' + '#'+Math.floor(Math.random()*16777215).toString(16) + ' 70%)';
+
+    box.style.background = 'linear-gradient(135deg, ' + '#'+Math.floor(Math.random()*16777215).toString(16) + ' 30%, ' + '#'+Math.floor(Math.random()*16777215).toString(16) + ' 70%)'  
+}
